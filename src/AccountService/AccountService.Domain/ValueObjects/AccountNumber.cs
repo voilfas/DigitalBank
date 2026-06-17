5061b7bc1e779936eaaebc5b50fe900b116b56e4
Teya- <sobkalovt@bk.ru>
2026-06-17 18:00:53 +0300

    feat(domain): add account number value object

---
+++ src/AccountService/AccountService.Domain/ValueObjects/AccountNumber.cs
@@ -0,0 +1,33 @@
+﻿using AccountService.Domain.Common;
+using AccountService.Domain.Errors;
+
+namespace AccountService.Domain.ValueObjects;
+
+public record AccountNumber
+{
+    public string Value { get; }
+
+    private AccountNumber(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<AccountNumber> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Result<AccountNumber>.Failure(AccountErrors.EmptyAccountNumber());
+
+        if (value.Length != 20)
+            return Result<AccountNumber>.Failure(AccountErrors.InvalidAccountNumber());
+
+        if (!value.All(char.IsDigit))
+            return Result<AccountNumber>.Failure(AccountErrors.InvalidAccountNumberFormat());
+
+        return Result<AccountNumber>.Success(new AccountNumber(value));
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
